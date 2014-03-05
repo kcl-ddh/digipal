4ba460e67d88b91c5fa94010391766af1d0f4e9e
gbuomprisco <giancarlopsk@gmail.com>
2014-03-05 13:14:05 +0000

    Fixed regex bug in filtering allographs

--- static/digipal/scripts/annotator-digipal.js
+++ static/digipal/scripts/annotator-digipal.js
@@ -404,7 +404,7 @@ DigipalAnnotator.prototype.filterAnnotation = function(checkboxes, formal_attrib
 	for (var i in features) {
 		if (formal_attribute == 'hand') {
 			attribute = features[i].hand;
-			attribute2 = features[i].feature.replace(/., |;. | /, '');
+			attribute2 = features[i].feature.replace(/\., |;\. | /, '');
 			hand = $('#hand_input_' + attribute);
 			allograph = $('#allograph_' + attribute2);
 			var allographs = $('.checkVectors');
@@ -417,7 +417,7 @@ DigipalAnnotator.prototype.filterAnnotation = function(checkboxes, formal_attrib
 				var max = allographs.length;
 				for (var h = 0; h < max; h++) {
 					var a = $(allographs[h]);
-					if (a.is(':checked') && a.val().replace(/., |;. | /, '') == attribute2) {
+					if (a.is(':checked') && a.val().replace(/\., |;\. | /, '') == attribute2) {
 						if ($(checkboxes).val() == attribute) {
 							features[i].style.fillOpacity = 0.4;
 							features[i].style.strokeOpacity = 1;
@@ -426,17 +426,17 @@ DigipalAnnotator.prototype.filterAnnotation = function(checkboxes, formal_attrib
 				}
 			}
 		} else {
-			attribute = features[i].feature.replace(/., |;. | /, '');
+			attribute = features[i].feature.replace(/\., |;\. | /, '');
 			attribute2 = features[i].hand;
 			hand = $('#hand_input_' + attribute2);
 			allograph = $('#hand_input_' + attribute2);
 			if (!($(checkboxes).is(':checked'))) {
-				if ($(checkboxes).val().replace(/., |;. | /, '') == attribute && features[i].hand == hand.val()) {
+				if ($(checkboxes).val().replace(/\., |;\. | /, '') == attribute && features[i].hand == hand.val()) {
 					features[i].style.fillOpacity = 0;
 					features[i].style.strokeOpacity = 0;
 				}
 			} else {
-				if ($(checkboxes).val().replace(/., |;. | /, '') == attribute && features[i].hand == hand.val() && hand.is(':checked')) {
+				if ($(checkboxes).val().replace(/\., |;\. | /, '') == attribute && features[i].hand == hand.val() && hand.is(':checked')) {
 					features[i].style.fillOpacity = 0.4;
 					features[i].style.strokeOpacity = 1;
 				}

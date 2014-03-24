a4d3e333c9f76d361c55d9d993a6bd6a7de5039c
gbuomprisco <giancarlopsk@gmail.com>
2014-03-24 15:27:09 +0000

    Improved parker-harvester script, added new commands, files get saved in folders by manuscript name

--- management/commands/parker-harvester.py
+++ management/commands/parker-harvester.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
+
 import urllib2, json, csv, os, base64, re
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from optparse import make_option
 
 class Command(BaseCommand):
@@ -15,74 +17,99 @@ class Command(BaseCommand):
     """
 
     option_list = BaseCommand.option_list + (
+        make_option('--command',
+            default='test',
+            help='Command to be executed'),
         make_option('--username',
             default='',
-            help='username to authenticate to the Parker On The Web server'),
+            help='Username to authenticate to the Parker On The Web server'),
         make_option('--password',
             default='',
-            help='password to authenticate to the Parker On The Web server'),
+            help='Password to authenticate to the Parker On The Web server'),
         make_option('--size',
             default='',
-            help='size of the images to download. Must be one among ["small", "medium", "large", "xlarge", "full", "thumb"]'),
+            help='Size of the images to download. Must be one among ["small", "medium", "large", "xlarge", "full", "thumb"]'),
         make_option('--csv-file',
             default=False,
             help='csv file to be parsed'),
         make_option('--output-folder',
             default=False,
-            help='folder where to store downloaded images'),
-        make_option('--dry-run',
-            default=False,
-            help='a boolean to specify whether download the images or not')
+            help='Folder where to store downloaded images'),
         )
 
     def handle(self, *args, **options):
 
-        self.username = args[0]
-        self.password = args[1]
-        self.size = args[2]
-        self.input_csv = args[3]
-        self.output_folder = args[4]
+        if len(args) < 1:
+            raise CommandError('Please provide a command. Try "python manage.py help parker-harvester" for help.')
 
-        if args < 4:
-            self.dry_run = True
-        else:
-            self.dry_run = args[5]
+        self.command = args[0]
+        self.username = args[1]
+        self.password = args[2]
+        self.size = args[3]
+        self.input_csv = args[4]
+        self.output_folder = args[5]
 
-        # allowed sizes provided by the Parker APIs
-        # if a wrong size is provided, raise an exception
+        # run download command
+        self.run_command()
+
+    def run_command(self):
+        # allowed sizes provided by the Parker APIs - if a wrong size is provided, raise an exception
         allowed_sizes = ["small", "medium", "large", "xlarge", "full", "thumb"]
 
         if self.size and self.size not in allowed_sizes:
             raise Exception('Size not valid. It should be one among these options:\n' + str(allowed_sizes))
 
-        csv_object = self.open_csv(self.input_csv)
+        csv_dict = self.open_csv(self.input_csv)
         url_json_manifest = 'http://dms-data.stanford.edu/data/manifests/Parker/collection.json'
 
-        # building array manuscripts contained in the csv file
-        manuscripts_csv = []
-        for i in csv_object:
-            manuscripts_csv.append(i)
+        manuscripts_csv = self.csv_to_array(csv_dict)
+        manuscripts_shelfmark_csv = self.extract_shelfmarks(manuscripts_csv)
+        selected_manuscripts = self.selected_manuscripts(manuscripts_csv)
 
+        manuscripts_collection = self.lookup_collection_manifest(url_json_manifest, manuscripts_shelfmark_csv)
+        self.lookup_manuscript_manifest(manuscripts_collection, selected_manuscripts)
+
+    def csv_to_array(self, csv_dict):
+        # building array manuscripts contained in the csv file
+
+        manuscripts_csv = []
+        for i in csv_dict:
+            manuscripts_csv.append(i)
+        return manuscripts_csv
+
+    def extract_shelfmarks(self, manuscripts_csv):
         manuscripts_shelfmark_csv = []
         for csv_manuscript in manuscripts_csv:
+
             shelfmark = re.sub(r'([\d]+)(.)*', r'\g<1>', csv_manuscript['Shelfmark'])
+
+            if len(shelfmark) == 1:
+                shelfmark = '00' + shelfmark
+            elif len(shelfmark) == 2:
+                shelfmark = '0' + shelfmark
             csv_manuscript_string = "%s MS %s" % (csv_manuscript['Repository'], shelfmark)
+
             if not csv_manuscript_string in manuscripts_shelfmark_csv:
                 manuscripts_shelfmark_csv.append(csv_manuscript_string)
+        return manuscripts_shelfmark_csv
 
+    def selected_manuscripts(self, manuscripts_csv):
         selected_manuscripts = {}
         for csv_manuscript in manuscripts_csv:
             label = csv_manuscript['Selected']
             shelfmark = re.sub(r'([\d]+)(.)*', r'\g<1>', csv_manuscript['Shelfmark'])
+
+            if len(shelfmark) == 1:
+                shelfmark = '00' + shelfmark
+            elif len(shelfmark) == 2:
+                shelfmark = '0' + shelfmark
+
             if not shelfmark in selected_manuscripts:
                 selected_manuscripts[shelfmark] = []
             if not label in selected_manuscripts[shelfmark]:
                 label = re.sub(r'(p)*[^\w]*', '', label).lower().replace(' ','')
                 selected_manuscripts[shelfmark].append(label)
-
-        manuscripts_collection = self.lookup_collection_manifest(url_json_manifest, manuscripts_shelfmark_csv)
-        self.lookup_manuscript_manifest(manuscripts_collection, selected_manuscripts)
-
+        return selected_manuscripts
 
     def open_csv(self, file_name):
 
@@ -117,29 +144,26 @@ class Command(BaseCommand):
 
 
     def lookup_manuscript_manifest(self, manuscripts_from_json, manuscripts_csv):
-        output = {}
-        m = 0
-        for manuscript in manuscripts_from_json[:5]:
+        for manuscript in manuscripts_from_json:
 
-            print '\n---------------------------------------------'
-            print 'Manuscript: %s' % (manuscript['manuscript'])
-            print 'Manifest URL: %s' % (manuscript['url'])
+            if self.command == 'test':
+                print '\n---------------------------------------------'
+                print 'Manuscript: %s' % (manuscript['manuscript'])
+                print 'Manifest URL: %s' % (manuscript['url'])
 
             request = urllib2.Request(manuscript['url'])
             json_file = urllib2.urlopen(request).read()
             json_object = json.loads(json_file)
             shelfmark = re.sub(r'CCCC MS ([\d]+)(.)*', r'\g<1>', manuscript['manuscript'])
+
             # parsing json file
             sequences = json_object['sequences']
-            if not shelfmark in output:
-                output[shelfmark] = []
+
             for sequence in sequences:
                 canvases = sequence['canvases']
                 for canvas in canvases:
                     value_canvas = re.sub(r'(p|f)*[^\w]*', '', canvas['label']).lower().replace(' ','')
-                    output[shelfmark].append(value_canvas)
                     if value_canvas in manuscripts_csv[shelfmark]:
-                        m += 1
                         resources = canvas['resources']
                         for resource in resources:
                             url = resource['resource']['@id']
@@ -147,24 +171,34 @@ class Command(BaseCommand):
                                 url += '_' + self.size
                             index = url.find('/image/') + len('/image/')
                             url = url[0:index] + 'app/' + url[index: len(url)]
-                            print "Label: ", canvas['label']
-                            print "URL: ", url
-                            if self.dry_run == "True":
-                                self.download(url, canvas['label'] + '.jpg')
 
-    def download(self, url, filename):
+                            if self.command == 'test':
+                                print "Label: ", canvas['label']
+                                print "URL: ", url
+                            if self.command == "download":
+                                self.download(url, canvas['label'] + '.jpg', manuscript['manuscript'])
 
-        # building request
-        request = urllib2.Request(url)
-        base64string = base64.encodestring('%s:%s' % (self.username, self.password))
-        authheader =  "Basic %s" % base64string
-        request.add_header("Authorization", authheader)
+    def download(self, url, file_name, folder_name):
 
         if not os.path.isdir(self.output_folder):
             os.mkdir(self.output_folder)
 
-        print os.path.isdir(self.output_folder)
-        # open a file and write image's data
-        image_file = open(self.output_folder + '/' + filename, 'wb')
-        image_data = urllib2.urlopen(request).read()
-        image_file.write(image_data)
+        if not os.path.isdir(self.output_folder + '/' + folder_name):
+            os.mkdir(self.output_folder + '/' + folder_name)
+
+        if not os.path.isfile(self.output_folder + '/' + folder_name + '/' + file_name):
+
+            # building request
+            request = urllib2.Request(url)
+            base64string = base64.encodestring('%s:%s' % (self.username, self.password))
+            authheader =  "Basic %s" % base64string
+            request.add_header("Authorization", authheader)
+
+            # open a file and write image's data
+            image_file = open(self.output_folder + '/' + folder_name + '/' + file_name, 'wb')
+            image_data = urllib2.urlopen(request).read()
+            image_file.write(image_data)
+            image_file.close()
+
+        else:
+            return False

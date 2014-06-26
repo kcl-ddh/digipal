2c77554cbdbeedda6ffcbc7b47de5423bcc81d78
Geoffroy Noel <geoffroy.noel@kcl.ac.uk>
2014-06-26 10:41:35 +0100

    Added digipal version in the build info line in the HTML headers.

--- digipal/processor.py
+++ digipal/processor.py
@@ -1,6 +1,10 @@
 from digipal.forms import SearchPageForm
+import digipal
 
 def quick_search(request):
     # We need this form for the quick search box
     # on to of every page
-    return {'quick_search_form': SearchPageForm()}
+    return {
+            'quick_search_form': SearchPageForm(),
+            'digipal_version': digipal.__version__
+            }

17feef8f84ff752a2977457f68a58393db7bcf37
geoffroy.noel@kcl.ac.uk <geoffroy.noel@kcl.ac.uk>
2015-02-27 12:58:27 +0000

    Fixed the url pattern for the text editor api. Apache doesn't like // e.g. /texts/translation/whole// , same with default.

--- digipal_text/urls.py
+++ digipal_text/urls.py
@@ -5,5 +5,8 @@ urlpatterns = patterns('digipal_text.views',
     url(r'^digipal/manuscripts/(\d+)/texts/view/$', 'viewer.text_viewer_view'),
     #url(r'^admin/digipal/itempart/(\d+)/edit/([^/]+)/$', 'admin.text_view'),
     
-    url(r'^digipal/manuscripts/(\d+)/texts/([^/]+)/([^/]+)/([^/]*)/$', 'viewer.text_api_view'),
+    # Replace the first line with the second as apache doesn't like // (e.g. /texts/translation/whole//)
+    # Django web server deals well with it. 
+    #url(r'^digipal/manuscripts/(\d+)/texts/([^/]+)/([^/]+)/([^/]*)/$', 'viewer.text_api_view'),
+    url(r'^digipal/manuscripts/(\d+)/texts/([^/]+)/([^/]+)/([^/]*)', 'viewer.text_api_view'),
 )

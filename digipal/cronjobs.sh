2124b1384a8bf1f195937228418e90e2052517e8
Geoffroy Noel <geoffroy.noel@kcl.ac.uk>
2015-07-27 12:01:28 +0100

    Don't reindex teh graphs everyday as it is takes too long.

--- digipal/cronjobs.sh
+++ digipal/cronjobs.sh
@@ -6,13 +6,13 @@ env_path="$1"
 DIR_PRJ=$( cd "$( dirname "$0" )" && pwd )/..
 cd $DIR_PRJ
 
-if [ -n "$env_path" ] 
-	then
-		source $env_path/bin/activate
-fi 
+if [ -n "$env_path" ]
+    then
+        source $env_path/bin/activate
+fi
 
 # Reindexing
 python manage.py dpsearch index
-python manage.py dpsearch index_facets
+python manage.py dpsearch index_facets --if=manuscripts,images,scribes,hands,texts
 
 deactivate

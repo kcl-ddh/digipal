8c7443ade3b0d24bb302233f36f9c9effd3ee8f3
Geoffroy Noel <geoffroy.noel@kcl.ac.uk>
2015-11-17 21:47:03 +0000

    WIP: OL3 annotations. Upgraded OL3 to 3.7.0. Added very basic box drawing to tester.

--- digipal_text/static/digipal_text/viewer/annotation.ts
+++ digipal_text/static/digipal_text/viewer/annotation.ts
@@ -1,18 +1,75 @@
 /// <reference path="../dts/jquery.d.ts"/>
+/// <reference path="../dts/openlayers.d.ts"/>
 
-class Greeter {
-    message: string;
+class AnnotatorOL3 {
+    map: ol.Map;
+    // source
+    source: ol.source.Vector;
+    // the annotation layer
+    layer: ol.layer.Vector;
+    //
+    interaction: ol.interaction.Draw;
    
-    constructor(message: string) {
-        this.message = message;
+    constructor(map: ol.Map) {
+        this.map = map;
+        
+        this.addAnnotationLayer();
+        
+        this.initInteractions();
     }
     
-    greet() {
-       alert(this.message);
+    addAnnotationLayer(): void {
+        // create layer
+        this.source = new ol.source.Vector({wrapX: false});
+        
+        this.layer = new ol.layer.Vector({
+          source: this.source,
+          style: new ol.style.Style({
+            fill: new ol.style.Fill({
+              color: 'rgba(255, 255, 255, 0.2)'
+            }),
+            stroke: new ol.style.Stroke({
+              color: '#ffcc33',
+              width: 2
+            }),
+            image: new ol.style.Circle({
+              radius: 7,
+              fill: new ol.style.Fill({
+                color: '#ffcc33'
+              })
+            })
+          })
+        });
+        
+        this.map.addLayer(this.layer);
     }
-}
+    
+    initInteractions(): void {
+        var geometryFunction, maxPoints;
+        var value = 'LineString';
+        maxPoints = 2;
+        geometryFunction = function(coordinates, geometry) {
+            if (!geometry) {
+                geometry = new ol.geom.Polygon(null);
+            }
+            var start = coordinates[0];
+            var end = coordinates[1];
+            geometry.setCoordinates([
+                [start, [start[0], end[1]], end, [end[0], start[1]], start]
+            ]);
+            return geometry;
+        };
 
-$(function() {
-    var greeter = new Greeter('Yo2!');
-    greeter.greet();
-});
+        if (this.interaction) this.map.removeInteraction(this.interaction);
+        
+        this.interaction = new ol.interaction.Draw({
+            source: this.source,
+            type: /** @type {ol.geom.GeometryType} */ (value),
+            geometryFunction: geometryFunction,
+            maxPoints: maxPoints
+        });
+        this.map.addInteraction(this.interaction);
+    }
+    
+    
+}

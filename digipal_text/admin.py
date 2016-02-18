66be334dffee536da5b40776c6b007c1984cac80
Geoffroy Noel <geoffroy.noel@kcl.ac.uk>
2016-02-18 19:51:32 +0000

    MOA-220: added admin filters to find duplicate and empty TExt Content XML records.

--- digipal_text/admin.py
+++ digipal_text/admin.py
@@ -28,7 +28,7 @@ class MessageWidget(forms.Widget):
         if value is None: value = ''
         from django.utils.safestring import mark_safe
         return mark_safe(value)
-    
+
 class MessageField(forms.Field):
     widget = MessageWidget
 
@@ -39,7 +39,7 @@ class MessageField(forms.Field):
 
     def set_form(self, form):
         self.parent_form = form
-        
+
     def prepare_value(self, value):
         ret = self.message
         if isCallable(ret):
@@ -73,23 +73,77 @@ class TextContentAdmin(reversion.VersionAdmin):
     list_display_links = list_display
     search_fields = ['item_part__display_label', 'text__name', 'languages__name', 'type__name']
     list_filter = ['languages', 'type']
-    
+
     fieldsets = (
             (None, {'fields': ('item_part', 'text', 'type', 'languages')}),
             ('Actions', {'fields': ('action_edit', )}),
             )
-    
+
 class TextContentTypeAdmin(reversion.VersionAdmin):
     model = TextContentType
+
+class FilterCTXDuplicate(SimpleListFilter):
+    title = 'Duplicates'
+
+    parameter_name = ('dup')
+
+    def lookups(self, request, model_admin):
+        return (
+            ('0', ('Unique')),
+            ('1', ('Duplicate')),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() in ['0', '1']:
+            duplicates = [r.id for r in TextContentXML.objects.raw('select tcx.* from digipal_text_textcontentxml tcx, digipal_text_textcontentxml tcx2 where tcx.id <> tcx2.id and tcx.text_content_id = tcx2.text_content_id')]
+            if self.value() == '1':
+                qs = queryset.filter(id__in=duplicates)
+            else:
+                qs = queryset.exclude(id__in=duplicates)
+            return qs
+
+class FilterCTXEmpty(SimpleListFilter):
+    title = 'Empty'
+
+    parameter_name = ('empty')
+
+    def lookups(self, request, model_admin):
+        return (
+            ('0', ('Not empty')),
+            ('1', ('Empty')),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() in ['0', '1']:
+            #qs = queryset.extra(select={'content_length': 'length(content)'})
+            cdt = ' > 2 '
+            if self.value() == '1':
+                cdt = ' < 3 OR content is null '
+            return queryset.extra(where=['length(content) ' + cdt])
 
 class TextContentXMLAdmin(reversion.VersionAdmin):
     model = TextContentXML
 
-    list_display = ['id', 'text_content', 'status', 'get_length', 'modified', 'created']
+    list_display = ['id', 'text_content', 'get_type_name', 'status', 'get_length', 'modified', 'created']
     list_display_links = ['id', 'text_content', 'status', 'modified', 'created']
     search_fields = ['id', 'text_content__item_part__display_label']
-    list_filter = ['status', 'text_content__languages', 'text_content__type']
-    
+    list_filter = ['status', 'text_content__languages', 'text_content__type', FilterCTXEmpty, FilterCTXDuplicate]
+
+    def queryset(self, request):
+        qs = super(TextContentXMLAdmin, self).queryset(request)
+        qs = qs.extra(select={'content_length': 'length(content)'})
+        return qs
+
+    def get_type_name(self, ctx):
+        return ctx.text_content.type.name
+    get_type_name.short_description = 'Type'
+    get_type_name.admin_order_field = 'text_content__type__name'
+
+    def get_length(self, tcx):
+        return tcx.content_length
+    get_length.short_description = 'Size'
+    get_length.admin_order_field = 'content_length'
+
 #     fieldsets = (
 #             (None, {'fields': ('item_part', 'text', 'type', 'languages')}),
 #             ('Actions', {'fields': ('action_edit', )}),

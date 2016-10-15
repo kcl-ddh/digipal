a9afb94cfcd7fae82eb5608d56fcf075b9d9da53
Geoffroy Noel <geoffroy.noel@kcl.ac.uk>
2016-10-15 20:45:39 +0100

    Added condition drop down to each pattern: ignore|exclude|include|process.

--- digipal_text/views/patterns.py
+++ digipal_text/views/patterns.py
@@ -1,20 +1,12 @@
 # -*- coding: utf-8 -*-
 # from digipal_text.models import *
-from digipal_text.models import TextContentXMLStatus, TextContent, TextContentXML
-import re
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
-from django.db import transaction
-from digipal import utils
 from django.utils.datastructures import SortedDict
-from django.conf import settings
 from digipal import utils as dputils
 from digipal_text.models import TextPattern
-import json
-
+import regex as re
 import logging
-from digipal.utils import sorted_natural, get_int_from_request_var
-from digipal.templatetags.hand_filters import chrono
+from digipal.utils import get_int_from_request_var
 from django.db.utils import IntegrityError
 from django.utils.text import slugify
 dplog = logging.getLogger('digipal_debugger')
@@ -30,6 +22,13 @@ def patterns_view(request):
     from exon.customisations.digipal_text.models import Entry
 
     context = {}
+    
+    context['conditions'] = [
+        {'key': '', 'label': 'May have'},
+        {'key': 'include', 'label': 'Must have'},
+        {'key': 'exclude', 'label': 'Must not have'},
+        {'key': 'ignore', 'label': 'Ignore'},
+    ]
 
     # arguments
     args = request.REQUEST
@@ -47,18 +46,22 @@ def patterns_view(request):
 
     cnt = 0
     for unit in Entry.objects.all():
-        # only transcriptions...
         cx = unit.content_xml
+        
+        # only transcription
         if cx.id != 4: continue
 
+        # only slected range
         if not is_unit_in_range(unit, context['units_range']): continue
 
-        # only first X units
-        #if context['units_limit'] > 0 and cnt >= context['units_limit']: break
-
         cnt += 1
-        context['units'].append(unit)
-    
+        # segment the unit
+        segment_unit(unit, context)
+
+        if unit.match_conditions:
+            context['units'].append(unit)
+
+    # stats    
     stats['result_size'] = len(context['units'])
     if context['units_limit'] > 0:
         context['units'] = context['units'][0:context['units_limit']]
@@ -73,15 +76,55 @@ def patterns_view(request):
 
     return ret
 
+def segment_unit(unit, context):
+    patterns = context['patterns']
+    unit.patterns = []
+    
+    unit.match_conditions = True
+    
+    for pattern_key, pattern in patterns.iteritems():
+        if not pattern.id: continue
+        if pattern.condition == 'ignore': continue
+        
+        # get regex from pattern
+        rgx = get_regex_from_pattern(patterns, pattern_key)
+        
+        # apply regex to unit
+        if rgx:
+            found = False
+            for match in rgx.finditer(unit.get_plain_content()):
+                found = True
+                unit.patterns.append([pattern_key, match.group(0)])
+            if (pattern.condition == 'include' and not found) or (pattern.condition == 'exclude' and found):
+                unit.match_conditions = False 
+        
+def get_regex_from_pattern(patterns, pattern_key):
+    ret = None
+    pattern = patterns.get(pattern_key, None)
+    
+    if pattern:
+        ret = getattr(pattern, 'rgx', None)
+        if ret is None:
+            ret = pattern.pattern
+            if ret:
+                # <person> habet <number> mansionem
+                ret = ret.replace(ur'<person>', ur'\w+')
+                ret = ret.replace(ur'<number>', ur'\.?[ivxlcm]+\.?')
+                print ret
+                ret = pattern.rgx = re.Regex(ret)
+    
+    return ret
+
 def update_patterns_from_request(request, context):
     # get patterns from DB as as sorted dictionary
     # {key: TextPattern}
     action = request.REQUEST.get('action', '')
 
     patterns = []
-    fields = ['title', 'pattern', 'key', 'order']
+    fields = ['title', 'pattern', 'key', 'order', 'condition']
     for pattern in (list(TextPattern.objects.all()) + [TextPattern.get_empty_pattern()]):
-        print 'pattern #%s' % pattern.id
+        #print 'pattern #%s' % pattern.id
+        pattern.condition = ''
 
         # modify the pattern from the request
         if action == 'update':
@@ -91,9 +134,10 @@ def update_patterns_from_request(request, context):
                 if field == 'key' and value:
                     value = slugify(value)
                 if value != getattr(pattern, field, ''):
-                    print '\t %s = %s' % (field, repr(value))
+                    #print '\t %s = %s' % (field, repr(value))
                     setattr(pattern, field, value)
-                    modified = True
+                    if field != 'condition':
+                        modified = True
 
             pattern.pattern = pattern.pattern.strip()
             if pattern.pattern:
@@ -110,7 +154,7 @@ def update_patterns_from_request(request, context):
                         raise
             else:
                 if pattern.id:
-                    print '\t DELETE'
+                    #print '\t DELETE'
                     pattern.delete()
                 pattern = None
 
@@ -121,9 +165,9 @@ def update_patterns_from_request(request, context):
     # make sorted dict
     context['patterns'] = SortedDict()
     new_order = 0
-    print patterns
+    #print patterns
     patterns = sorted(patterns, key=lambda p: p.order)
-    print patterns
+    #print patterns
     for pattern in patterns:
         new_order += 1
         pattern.order = new_order

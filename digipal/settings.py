3b3c0a978442f7a1b869fa5158f1fbbcb601bcf2
Geoffroy Noel <geoffroy.noel@kcl.ac.uk>
2016-10-16 20:25:37 +0100

    WIP: pattern analysis. Function to load all plain text units from cache.

--- digipal/settings.py
+++ digipal/settings.py
@@ -619,6 +619,13 @@ CACHES = {
         'TIMEOUT': 60*60*24,
         #'TIMEOUT': 1,
         'MAX_ENTRIES': 300,
+    },
+    'digipal_text_patterns': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': os.path.join(PROJECT_ROOT, 'django_cache/text_patterns/'),
+        'TIMEOUT': 60*60*24,
+        #'TIMEOUT': 1,
+        'MAX_ENTRIES': 300,
     }
 }
 

c9b68b91897167b53b313877940ab64982aeb1d7
Geoffroy Noel <geoffroy.noel@kcl.ac.uk>
2017-07-12 10:59:32 +0100

    Created default project/customisation folder.

---
+++ digipal_cust/local_settings_template.py
@@ -0,0 +1,42 @@
+# Template file for local settings.
+# Do NOT change settings.py instead copy this to local_settings.py, change it
+# to suite the environment, but do NOT commit it to version control.
+DEBUG = True
+
+PROJECT_URL = '/'
+
+ADMINS = (
+    # ('Your Name', 'your_email@domain.com'),
+)
+
+# Remove this line to work with JP2 format
+# Note that it requires a kakadu license for commercial applications
+IMAGE_SERVER_EXT = 'tif'
+
+IMAGE_SERVER_HOST = 'localhost'
+IMAGE_SERVER_ROOT = '/home/digipal/images/'
+IMAGE_URLS_RELATIVE = True
+
+IMAGE_SERVER_ZOOMIFY = 'http://%s%s?zoomify=%s/'
+IMAGE_SERVER_PATH = '/iip/iipsrv.fcgi'
+IMAGE_SERVER_URL = 'http://%s%s' % (IMAGE_SERVER_HOST, IMAGE_SERVER_PATH)
+
+MANAGERS = ADMINS
+STATICFILES_DIRS = (
+    # Put strings here, like '/home/html/static' or 'C:/www/django/static'.
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+#SHOW_QUICK_SEARCH_SCOPES = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'archetype',
+        'USER': 'app_archetype',
+        'PASSWORD': 'pass',
+        'HOST': '',
+        'PORT': '',
+    },
+}
---
+++ digipal_cust/settings.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+#######################################################################
+# This file is for project customisation ONLY                         #
+# General DigiPal settings go to digipal/settings.py                  #
+# Particular server instance settings go to local_settings.py         #
+#######################################################################
+
+# Import the settings from digipal
+from digipal.settings import *

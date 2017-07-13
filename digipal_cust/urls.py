8fabd0049276673b12e809eb3f522e01509e4b48
Geoffroy Noel <geoffroy.noel@kcl.ac.uk>
2017-07-13 17:26:20 +0100

    added missing digipal_cust/urls.py

--- digipal_cust/urls.py
+++ digipal_cust/urls.py
@@ -1,12 +1,8 @@
 from mezzanine.conf import settings
 from django.conf.urls import patterns, include, url
 from mezzanine.core.views import direct_to_template
-from redirects import get_redirect_patterns
 from django.contrib import admin
 admin.autodiscover()
-
-# redirects
-urlpatterns = get_redirect_patterns()
 
 # ADD YOUR OWN URLPATTERNS *ABOVE* THE LINE BELOW.
 # DigiPal URLs

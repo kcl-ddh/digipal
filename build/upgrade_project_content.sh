466296e4a34a30e7636de87608f651e8c7875940
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2017-08-24 02:37:40 +0100

    Improved upgarde_project_content.sh so it pull digipal_project folder content from local repo in case the user has enabled the volume on the host OS (e.g. with Kitematic). Also reset the permission each time to make sure both container.www-data and host:user can RW

--- build/upgrade_project_content.sh
+++ build/upgrade_project_content.sh
@@ -1,5 +1,14 @@
-# Upgrade imported project content into this version of Archetype
 cd /home/digipal
-python manage.py colectstatic --noinput
-python manage.py migrate --fake-initial
-python manage.py migrate
+# recreate content of digipal_project if empty (e.g. enabled volume in kitematic)
+if [ ! -e "digipal_project/__init__.py" ]; then
+    git checkout digipal_project
+fi
+# Upgrade imported project content into this version of Archetype
+/etc/init.d/postgresql start && python manage.py collectstatic --noinput && python manage.py migrate --fake-initial && python manage.py migrate
+# we don't need django/psql
+/etc/init.d/postgresql stop
+# reset permission to www-data
+chown -R www-data:www-data ../digipal && chmod -R ug+rw ../digipal && chmod -R o-rw ../digipal
+# grant permissions to share folder to world
+chmod o+rw -R digipal_project
+

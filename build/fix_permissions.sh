f0cc1792ee3790c97c2a4da4b99be58e16791fef
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2017-08-26 09:55:52 +0100

    WIP: fixed permissions of sh in build folder.

--- build/fix_permissions.sh
+++ build/fix_permissions.sh
@@ -10,7 +10,10 @@ chmod o+rw -R digipal_project
 chmod o+x digipal_project
 
 # special permission requirements for psql on its database folder
-chown postgres:postgres -R digipal_project/database
-chmod u+rw -R digipal_project/database
-chmod u+x digipal_project/database
-chmod go-rwx digipal_project/database
+# special permission requirements for psql on its database folder
+if [ -e "digipal_project/database" ]; then
+    chown postgres:postgres -R digipal_project/database
+    chmod u+rw -R digipal_project/database
+    chmod u+x digipal_project/database
+    chmod go-rwx digipal_project/database
+fi

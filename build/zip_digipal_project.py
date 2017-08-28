9c7e254b6c15dc13eefd706045c029eecdc86d98
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2017-08-28 15:16:26 +0100

    WIP: docker/upgrade. zip_digipal_project.py now creates a zip file with almost all the required content.

--- build/zip_digipal_project.py
+++ build/zip_digipal_project.py
@@ -1,10 +1,19 @@
+# How to make a backup of your framework content:
+# got to the terminal (On Kitematic, click Exec when DigiPal/Archetype is running)
+# then type/paste these lines (press ENTER/RETURN key at then end of each):
 # apt-get install wget
 # wget -O zip_digipal_project.py http://bit.ly/2xERjk7
 # python zip_digipal_project.py
+# exit
+#
+# TODO:
+# Don't leave backup in static (or use random number for folder name)
+# use plain zip
+
 import os
 import re
 import sys
-from subprocess import call, check_output
+from subprocess import check_output
 
 
 def run_cmd(command):
@@ -12,39 +21,117 @@ def run_cmd(command):
     return res
 
 
-manage_path = run_cmd('find .. -name manage.py | tail -n 1')
-manage_path = manage_path.strip('\n')
+class ProjectZipper(object):
 
-print 'manage.py: ' + manage_path
-os.chdir(os.path.dirname(manage_path))
+    def import_settings(self):
+        manage_path = run_cmd('find .. -name manage.py | tail -n 1')
+        manage_path = manage_path.strip('\n')
 
-settings_module = run_cmd('grep "DJANGO_SETTINGS_MODULE" ' + manage_path)
-eval(re.sub(r'(?musi)^\s+', '', settings_module))
-settings_module = os.environ['DJANGO_SETTINGS_MODULE']
+        print 'manage.py: ' + manage_path
+        os.chdir(os.path.dirname(manage_path))
 
-print 'setting: ' + settings_module
-from importlib import import_module
-sys.path.append('.')
-settings = import_module(settings_module)
+        settings_module = run_cmd(
+            'grep "DJANGO_SETTINGS_MODULE" ' + manage_path)
+        eval(re.sub(r'(?musi)^\s+', '', settings_module))
+        settings_module = os.environ['DJANGO_SETTINGS_MODULE']
 
-sql_filename = 'archetype.sql'
-sql_path = os.path.join(settings.STATIC_ROOT, sql_filename)
+        print 'setting: ' + settings_module
+        from importlib import import_module
+        sys.path.append('.')
+        self.settings = import_module(settings_module)
 
-DB = settings.DATABASES['default']
+    def get_dst_path(self, part=None):
+        ret = os.path.join(self.settings.STATIC_ROOT)
+        if part:
+            ret = os.path.join(ret, part)
+        return ret
 
-host = DB['HOST']
-port = DB['PORT']
-if port:
-    port = '-p ' + port
-if host:
-    host = '-h ' + host
-username = DB['USER']
-dbname = DB['NAME']
-password = DB['PASSWORD']
-os.environ['PGPASSWORD'] = password
-command = 'pg_dump -U %s %s -c %s %s > "%s"' % (
-    username, dbname, host, port, sql_path)
-os.system(command)
-os.system('chmod ugo+rw %s' % sql_path)
+    def dump_database(self):
+        settings = self.settings
+        sql_filename = 'archetype.sql'
+        sql_path = self.get_dst_path(sql_filename)
 
-print os.path.join(settings.STATIC_URL, sql_filename)
+        DB = settings.DATABASES['default']
+
+        host = DB['HOST']
+        port = DB['PORT']
+        if port:
+            port = '-p ' + port
+        if host:
+            host = '-h ' + host
+        username = DB['USER']
+        dbname = DB['NAME']
+        password = DB['PASSWORD']
+        os.environ['PGPASSWORD'] = password
+        command = 'pg_dump -U %s %s -c %s %s > "%s"' % (
+            username, dbname, host, port, sql_path)
+        os.system(command)
+
+        self.add_path_to_tar(sql_path)
+
+        run_cmd('rm -f %s' % sql_path)
+
+        print 'Database saved into %s' % sql_path
+        # print 'Download it at %s' % os.path.join(settings.STATIC_URL,
+        # sql_filename)
+
+    def get_tar_path(self):
+        return os.path.join(self.get_dst_path(), 'archetype.tar')
+
+    def add_path_to_tar(self, path, new_name=None):
+        path = path.rstrip('/')
+
+        if os.path.exists(path):
+            if new_name:
+                new_path = os.path.join('/tmp', new_name)
+                run_cmd('ln -fs %s %s' % (path, new_path))
+                path = new_path
+
+            run_cmd('tar --append -f %s -C %s %s' %
+                    (self.get_tar_path(), os.path.dirname(path), os.path.basename(path)))
+
+            if new_name:
+                run_cmd('unlink %s' % new_path)
+
+    def create_tar(self):
+        run_cmd('tar -cf %s --files-from /dev/null' % self.get_tar_path())
+
+    def zip_project(self):
+        self.import_settings()
+
+        self.create_tar()
+
+        # database
+        self.dump_database()
+
+        # customisations
+        self.add_path_to_tar(os.path.join(
+            self.settings.PROJECT_ROOT, 'customisations'))
+        # templates
+        self.add_path_to_tar(os.path.join(
+            self.settings.PROJECT_ROOT, 'templates'))
+        # media
+        self.add_path_to_tar(self.settings.MEDIA_ROOT, 'media')
+        # images
+        self.add_path_to_tar(self.settings.IMAGE_SERVER_ROOT, 'images')
+
+        # TODO: local_settings.py, settings.py and urls.py
+        self.add_path_to_tar(os.path.join(
+            self.settings.PROJECT_ROOT, 'settings.py'), 'settings.py.bk')
+        self.add_path_to_tar(os.path.join(
+            self.settings.PROJECT_ROOT, 'local_settings.py'), 'local_settings.py.bk')
+        self.add_path_to_tar(os.path.join(
+            self.settings.PROJECT_ROOT, 'urls.py'), 'urls.py.bk')
+
+        # Now zip it all
+        run_cmd('gzip -f9 %s' % self.get_tar_path())
+# run_cmd('tar -cf %s.tar -C %s archetype.sql' % (self.get_dst_path(),
+# self.get_dst_path()
+
+        print 'Download your backup at: %s' % (os.path.join(self.settings.STATIC_URL, 'archetype.tar.gz'), )
+#                                                         ))
+        # os.system('chmod ugo+rw %s.tar' % self.get_dst_path())
+
+
+zipper = ProjectZipper()
+zipper.zip_project()

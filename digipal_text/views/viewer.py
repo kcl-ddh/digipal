e836836a7eac743d32b9e03bd437777546d3c2d9
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2020-05-29 22:35:01 +0100

    Made the text viewer MAX_FRAGMENT_SIZE a settings.py variable so it can customised for each project.

--- digipal_text/views/viewer.py
+++ digipal_text/views/viewer.py
@@ -16,7 +16,8 @@ from digipal.utils import sorted_natural
 from digipal.templatetags.hand_filters import chrono
 dplog = logging.getLogger('digipal_debugger')
 
-MAX_FRAGMENT_SIZE = 60000
+MAX_FRAGMENT_SIZE = getattr(settings, 'MAX_FRAGMENT_SIZE', 60000)
+print(MAX_FRAGMENT_SIZE)
 
 
 def can_user_see_main_text(itempart, request):
